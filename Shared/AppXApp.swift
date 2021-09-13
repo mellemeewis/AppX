7fa0a0a4f52ac8d0810fc5948f64bb1a83de6840
Melle meewis <mellemeewi@icloud.com>
2021-09-13 23:05:42 +0200

    Initial Commit

---
+++ Shared/AppXApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppXApp.swift
+//  Shared
+//
+//  Created by Melle Meewis on 13/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
